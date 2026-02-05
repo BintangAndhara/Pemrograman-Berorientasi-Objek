f7e144d251509a6c2a0555b115799183536b9acf
BintangAndhara <bintangandha1745@gmail.com>
2026-02-05 14:21:28 +0700

    Pertemuan1

---
+++ BintangAndharaPutra_D1041241051_4.java
@@ -0,0 +1,8 @@
+public class BintangAndharaPutra_D1041241051_4 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Bintang Andhara Putra");
+        System.out.println("Nim : D1041241051");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
