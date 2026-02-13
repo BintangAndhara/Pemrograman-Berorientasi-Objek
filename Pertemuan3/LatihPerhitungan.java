a059c8e18fc475c4daaa25a384413cdbac68cfdb
BintangAndhara <bintangandha1745@gmail.com>
2026-02-13 08:09:36 +0700

    Latihan 2 Pertemuan 3

--- Pertemuan3/LatihPerhitungan.java
+++ Pertemuan3/LatihPerhitungan.java
@@ -2,16 +2,36 @@ package Pertemuan3;
 
 public class  LatihPerhitungan {
     public static void main (String[] args) {
-        int saldo = 1000000, SaldoAkhir;
-        System.out.println("Saldo Awal             : " + saldo);
-        int setoran = 500000;
-        System.out.println("Setoran                : " + setoran);
-        SaldoAkhir = saldo + setoran;
-        System.out.println("Saldo Setelah Setor    : " + SaldoAkhir);
-        int penarikan = 250000;
-        System.out.println("Penarikan              : " + penarikan);
-        SaldoAkhir = SaldoAkhir - penarikan;
-         System.out.println("Saldo Akhir           : " + SaldoAkhir);
+      int saldo = 1000000, SaldoAkhir; 
+// Deklarasi saldo awal dan variabel untuk menyimpan saldo akhir
+
+System.out.println("Saldo Awal             : " + saldo);
+// Menampilkan saldo awal
+
+int setoran = 500000; 
+// Nominal uang yang disetor
+
+System.out.println("Setoran                : " + setoran);
+// Menampilkan jumlah setoran
+
+SaldoAkhir = saldo + setoran; 
+// Menghitung saldo setelah ditambah setoran
+
+System.out.println("Saldo Setelah Setor    : " + SaldoAkhir);
+// Menampilkan saldo setelah setor
+
+int penarikan = 250000; 
+// Nominal uang yang ditarik
+
+System.out.println("Penarikan              : " + penarikan);
+// Menampilkan jumlah penarikan
+
+SaldoAkhir = SaldoAkhir - penarikan; 
+// Mengurangi saldo dengan jumlah penarikan
+
+System.out.println("Saldo Akhir            : " + SaldoAkhir);
+// Menampilkan saldo akhir setelah semua transaksi
+
     }
 }
 //Nama : Bintang Andhara Putra
