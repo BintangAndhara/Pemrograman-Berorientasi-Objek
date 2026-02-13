36429ce6e6489ec420394c289f0c1087b5e851b0
BintangAndhara <bintangandha1745@gmail.com>
2026-02-13 08:06:52 +0700

    Latihan 1 Pertemuan 3

---
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -0,0 +1,24 @@
+package Pertemuan3;
+
+public class LatihDataMahasiswa {
+    public static void main (String[] args) {
+        long nim = 2301010001L;
+        String nama = "Budi Santoso";
+        char kelas = 'A';
+        boolean status = true;
+        double ipk = 3.75;
+        int sks = 72;
+
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.println("NIM               : " + nim);
+        System.out.println("NAMA              : " + nama);
+        System.out.println("KELAS             : " + kelas);
+        System.out.println("Status Aktif      : " + status);
+        System.out.println("IPK Semester      : " +  ipk);
+        System.out.println("Total SKS         : " + sks);
+
+    }
+}
+//Nama : Bintang Andhara Putra
+//Nim : D1041241051
+//Hari/tangggal : Kamis,5 Febuari 2026
---
+++ Pertemuan3/tugas3.java
@@ -0,0 +1,11 @@
+package Pertemuan3;
+
+public class tugas3 {
+    public static main void (String[] args) {
+        
+    }
+}
+
+//Nama : Bintang Andhara Putra
+//Nim : D1041241051
+//Hari/tangggal : Kamis,5 Febuari 2026
