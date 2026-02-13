ea8b352ba63b496b3685429afdf2140218a52aa4
BintangAndhara <bintangandha1745@gmail.com>
2026-02-13 08:11:13 +0700

    Latihan 1 Pertemuan 3

--- Pertemuan3/LatihDataMahasiswa.java
+++ Pertemuan3/LatihDataMahasiswa.java
@@ -1,13 +1,24 @@
 package Pertemuan3;
 
 public class LatihDataMahasiswa {
-    public static void main (String[] args) {
-        long nim = 2301010001L;
-        String nama = "Budi Santoso";
-        char kelas = 'A';
-        boolean status = true;
-        double ipk = 3.75;
-        int sks = 72;
+        public static void main (String[] args) {
+        long nim = 2301010001L; 
+        // NIM menggunakan tipe long karena angkanya besar (huruf L wajib di akhir)
+
+        String nama = "Budi Santoso"; 
+        // Nama mahasiswa bertipe String (teks)
+
+        char kelas = 'A'; 
+        // Kelas menggunakan tipe char karena hanya satu karakter
+
+        boolean status = true; 
+        // Status aktif atau tidak (true = aktif, false = tidak aktif)
+
+        double ipk = 3.75; 
+        // IPK menggunakan double karena memiliki angka desimal
+
+        int sks = 72; 
+        // Total SKS menggunakan tipe int karena berupa bilangan bulat
 
         System.out.println("=== Data Mahasiswa ===");
         System.out.println("NIM               : " + nim);
