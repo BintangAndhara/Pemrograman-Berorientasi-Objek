cc2475cfef45ee2eb75020541c6f8a180483a993
BintangAndhara <bintangandha1745@gmail.com>
2026-02-20 09:17:28 +0700

    Praktikum 1 Pertemuan 9

---
+++ Pertemuan9/ObjekdanClass.java
@@ -0,0 +1,21 @@
+package Perteman9;
+ public class ObjekdanClass {
+    static class Mobil {
+        String merk;
+        String warna;
+        int tahun;
+        int kecepatan;
+    }
+        public static void main(String[] args) {
+        Mobil mobils = new Mobil();
+        mobils.merk = "Lamborghini";
+        mobils.warna = "Biru";
+        mobils.tahun = 2026;
+        mobils.kecepatan = 500;
+
+        System.out.println("Mobil: " + mobils.merk);
+        System.out.println("Warna: " + mobils.warna);
+        System.out.println("Tahun: " + mobils.tahun);
+        System.out.println("Kecepatan: " + mobils.kecepatan);
+    }
+}
